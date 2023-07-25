a508af968530130ddec269acc49f7e0e807ded4e
Chip Reed <chip@chip.sh>
2023-07-25 12:59:50 +0200

    remove unnecessary (i think) watch dir

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
 fn main() {
-    // Only watch the `dist/` directory for recompiling, preventing unnecessary
-    // changes when we change files in other project subdirectories.
-    println!("cargo:rerun-if-changed=dist");
-
     // Run the Tauri build-time helpers
     tauri_build::build()
 }
